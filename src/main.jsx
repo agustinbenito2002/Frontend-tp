8456ae8cb3d6901456d89ddb138fe14bd1399839
Agustin Benito <agusbenito2002@gmail.com>
2025-11-04 20:58:23 -0300

    2da

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios'
+
+const promise = axios.get('https://localhost:3000/api/duenios')
+console.log(promise)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
